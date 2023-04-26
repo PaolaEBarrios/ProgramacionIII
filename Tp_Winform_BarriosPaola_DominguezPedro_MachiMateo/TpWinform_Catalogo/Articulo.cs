a098477f378f57cb915ac91f1217cc4345b19abb
Paola Barrios <pbarrios0795@gmail.com>
2023-04-26 20:18:14 -0300

    Iniciando

---
+++ Tp_Winform_BarriosPaola_DominguezPedro_MachiMateo/TpWinform_Catalogo/Articulo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TpWinform_Catalogo
+{
+    internal class Articulo
+    {
+        public int codigoArticulo { get; set; }
+        public string nombre { get; set; }
+        public string descripcion { get; set; }
+        public Marca Marca { get; set; }
+        public  Categoria Categoria { get; set; }
+        public string imagen { get; set; }
+        public decimal precio { get; set; }
+
+    
+
+        
+
+    }
+}
---
+++ Tp_Winform_BarriosPaola_DominguezPedro_MachiMateo/TpWinform_Catalogo/ArticuloNegocio.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data.SqlClient;
+
+
+namespace TpWinform_Catalogo
+{
+    internal class ArticuloNegocio
+    {
+        public List<Articulo> listar()
+        {
+            List<Articulo> lista = new List<Articulo>();
+            //OBJETO CONECTAR CONECTAR
+            SqlConnection conexion = new SqlConnection();
+            //OBJETO COMANDO PARA REALIZAR ACCIONES
+            SqlCommand comando = new SqlCommand();
+            //OBJETO  RESULTADO, SET DE DATOS LECTOR
+            SqlDataReader lector;
+
+
+            try
+            {
+                conexion.ConnectionString = "server=.\\SQLEXPRESS; database=;integrated security=true";
+                comando.CommandType = System.Data.CommandType.Text;
+                comando.CommandText = "";
+
+                comando.Connection = conexion;
+                conexion.Open();
+
+                lector=comando.ExecuteReader();
+
+                while(lector.Read())
+                {
+                    Articulo aux = new Articulo();
+                    aux.imagen =
+                    aux.codigoArticulo =
+                    aux.descripcion =
+                    aux.precio =
+                    aux.nombre =
+                    aux.Marca.marca =
+                    aux.Categoria.categoria =
+
+                    lista.Add(aux);
+                }
+
+                return lista;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            
+            
+               
+        }
+    }
+}
